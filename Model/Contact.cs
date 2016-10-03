89427ee83c88bd539e95cf20993d69aa46f01dd4
SkyLi7h <skyli7h@gmail.com>
2016-10-03 09:32:28 +0200

    Add project files.

---
+++ Model/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contact
+    {
+        public String FirstName
+        {
+            get;
+            set;
+        }
+        public String LastName
+        {
+            get;
+            set;
+        }
+        public String Email
+        {
+            get;
+            set;
+        }
+
+        public Contact(String fn, String ln, String e)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Email = e;
+        }
+    }
+}
